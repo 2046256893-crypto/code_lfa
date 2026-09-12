51625c6e9c3923a1b36a281ac7e2f83527427375
2046256893-crypto <2046256893@qq.com>
2026-09-12 18:22:40 +0800

    Update terminal_page.dart
    sjjsjs

--- lib/terminal_page.dart
+++ lib/terminal_page.dart
@@ -1,9 +1,11 @@
 import 'package:code_lfa/utils.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:global_repository/global_repository.dart';
 import 'package:xterm/xterm.dart';
+
 import 'terminal_controller.dart';
 import 'terminal_theme.dart';
 
